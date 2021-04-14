85255c7f4912c502bcc0afc1e028996e499d97df
Austin Green <austinjgreen94@yahoo.com>
2021-04-14 17:56:22 -0400

    initial skeleton commit

---
+++ doc.go
@@ -0,0 +1,5 @@
+package internationalcolortime
+
+// InternationalColorTime is a time standard developed by Dave Grossman and published sometime circa 2010. It uses the
+// idea of a looping color value with no clear high or low point to replace the outdated concept of time being
+// represented by incrementing integers that wrap around.
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/katamarijr/internationalcolortime
+
+go 1.15
