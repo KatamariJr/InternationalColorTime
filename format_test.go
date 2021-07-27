05dc5d48250d53fa6481a4183917f876ed9f3d99
Austin Green <austinjgreen94@yahoo.com>
2021-07-26 20:13:27 -0400

    add some extra formatting tests

--- format_test.go
+++ format_test.go
@@ -21,12 +21,44 @@ func TestInternationalColorTime_Format(t *testing.T) {
 			want: "Blue:15:00",
 		},
 		{
+			name:  "Long Name 2",
+			field: ColorTime(Pink, 59, 33, 0),
+			args: args{
+				layout: ICTStandardLongName,
+			},
+			want: "Pink:59:33",
+		},
+		{
+			name:  "Long Name 3 w/ nanos",
+			field: ColorTime(Denim, 22, 17, 999000000),
+			args: args{
+				layout: ICTStandardLongName,
+			},
+			want: "Denim:22:17.999",
+		},
+		{
 			name:  "Short Name",
 			field: ColorTime(Blue, 15, 0, 0),
 			args: args{
 				layout: ICTStandardShortName,
 			},
 			want: "BLU:15:00",
+		},
+		{
+			name:  "Short Name 2",
+			field: ColorTime(Green, 45, 22, 0),
+			args: args{
+				layout: ICTStandardShortName,
+			},
+			want: "GRN:45:22",
+		},
+		{
+			name:  "Short Name 3 w/ nanos",
+			field: ColorTime(Yellow, 5, 2, 987623443),
+			args: args{
+				layout: ICTStandardShortName,
+			},
+			want: "YLW:05:02.987623443",
 		},
 	}
 	for _, tt := range tests {
